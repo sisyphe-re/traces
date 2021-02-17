30371909ea50e59405696ab94ceffa5cbf77a6d3
ns <nina.santi@protonmail.com>
2021-02-17 15:26:26 +0100

    Complete python src

---
+++ python_src/main.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import matplotlib.pyplot as plt
+
+from shutil import copyfile
+from nodes import Node, MessageTx, MessageRx
+from nodes import averageTransmissionLatency, packetDeliveryRatio
+
+
+def parseLog(log_filename):
+    """Take a log file an return a list of Node found in it 
+
+    Parameters
+    ----------
+    log_filename 
+        the name of the file containing the experiment's log
+    
+    Returns
+    -------
+    list
+        a list of Nodes from the log file
+    """
+
+    try:
+       log_file = open(log_filename, "r" )
+    except IOError:
+        print(sys.argv[0]+": "+log_filename+": cannot open file")
+        sys.exit(3)
+
+    lines = log_file.readlines()
+    nodes_dict = {}
+    for line in lines:
+        receiver = "Received" in line
+        sender = "Sending" in line
+        if not sender and not receiver :
+            continue
+        try:
+            split = line.split(';')
+            timestamp = float(split[0])*1000 # Convert in milliseconds
+            name = split[1]
+            msg = split[2]
+            msg_id = int(split[3])
+
+            # If we find a new node
+            if not name in nodes_dict:
+                # We create a new Node in the nodes dict
+                nodes_dict[name] = Node(name)
+
+            if receiver :
+                nodes_dict[name].list_rx.append(MessageRx(timestamp, msg_id, name))
+            # If sender 
+            else :
+                nodes_dict[name].list_tx.append(MessageTx(timestamp, msg_id, name))
+
+        except IndexError as e:
+            print("Bad formatted line : {}".format(e))
+
+    nodes_list = nodes_dict.values()
+    return nodes_list
+
+
+def beautifyData(exp_name, nodes_list):
+    """Write in file the raw data in a more readable way
+    It will write all the transmitted messages in a filenameRx.data
+    and received messages in a filenameTx.data
+
+    The two file are store in a directory with the experience's name
+    
+    Format of the Rx.data :
+        transmitted;<node name>;<timestamp>;<message id>;<transmission success>
+
+    Format of the Tx.data :
+        received;<node name>;<timestamp>;<message id>;<reception delay>
+
+    where
+        <transmission success> is the nb of node that have received this message
+        <reception delay> is the time that the message has taken to reach the node in milliseconds
+
+    Parameters
+    ----------
+    exp_name 
+        the experience's name used to name and store the beautified data 
+    nodes_list
+        a list of Node
+
+    Notes
+    -----
+    
+    """
+    if not os.path.exists(exp_name):
+        os.makedirs(exp_name)
+
+    rx_name = "{}/{}Rx.data".format(exp_name, exp_name)
+    tx_name = "{}/{}Tx.data".format(exp_name, exp_name)
+
+    with open(rx_name, "w") as rx_file, open(tx_name, "w") as tx_file: 
+        tx_file.write("transmitted;node name;timestamp;message id;transmission success\n")
+        rx_file.write("received;node name;timestamp;message id;reception delay\n")
+        for n in nodes_list:
+            for t in n.list_tx:
+                tx_file.write('{}\n'.format(t))
+            for r in n.list_rx:
+                rx_file.write('{}\n'.format(r))
+
+def generatePlots(exp_name, nodes_list):
+    """Generate plots from the list of nodes
+
+    Generate a graph for the packet delivery ratio of each nodes
+    and a graph for the average transmission latency of each nodes in miliseconds
+
+    Parameters
+    ----------
+    exp_name 
+        the name of the experience, used to store the plot
+        in the corresponding directory
+    nodes_list
+        a list of Node
+
+    Notes
+    -----
+    Packet delivery ratio = ∑ Number of packet received / ∑ Number of packet send
+    
+    """
+
+    if not os.path.exists(exp_name):
+        os.makedirs(exp_name)
+
+    names = []
+    pdr = []
+    latency = []
+
+    plt.rcdefaults()
+
+    for n in nodes_list:
+        names.append(n.name)
+        pdr.append(n.pdr)
+        latency.append(n.latency)
+
+    # Plot pdr 
+    fig = plt.figure(figsize=(15, 15))
+    plt.xticks(rotation='vertical')
+    plt.bar(names, pdr, width=0.3)
+    plt.xlabel("Node ID")
+    plt.ylabel("PDR (%)")
+    plt.title("Packet Delivery Ratio (PDR) per node")
+    #plt.show()
+    plt.savefig(exp_name + "/PacketDeliveryRatio.png")
+
+
+    # Plot latency 
+    fig = plt.figure(figsize=(14, 15))
+    plt.xticks(rotation='vertical')
+    plt.bar(names, latency, align="edge", width=0.3)
+    plt.xlabel("Node ID")
+    plt.ylabel("Average Latency (ms)")
+    plt.title("Average packet transmission latency per node")
+    #plt.show()
+    plt.savefig(exp_name + "/AverageTransmissionLatency.png")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3 :
+        print("Usage: "+sys.argv[0]+" <log filename> <exp name>")
+        sys.exit(2)
+    log_filename = sys.argv[1]
+    exp_name = sys.argv[2]
+
+    nodes_list = parseLog(log_filename)
+    packetDeliveryRatio(nodes_list)
+    averageTransmissionLatency(nodes_list)
+
+    beautifyData(exp_name, nodes_list)
+    generatePlots(exp_name, nodes_list)
+
+    # Just copy the rawdata in the same directory as beautified data
+    if not os.path.exists(exp_name):
+        os.makedirs(exp_name)
+    copyfile(log_filename, "{}/{}.rawdata".format(exp_name, exp_name))
---
+++ python_src/nodes.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+
+class Node:
+    """
+    Represent a node in the log file
+    
+    Attributes
+    ----------
+    name : string
+        the node's name
+    list_rx : list
+        list of received messages
+    list_tx : list
+        list of transmitted messages
+    pdr : int
+        the packet delivery ratio of this node
+    latency : float
+        the average transmission latency of this node
+    success : int 
+        total of the received msg that have been send by this node
+
+    Notes
+    -----
+    Packet delivery ratio = ∑ Number of packet received / ∑ Number of packet send
+
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.list_rx = [] 
+        self.list_tx = []
+        self.pdr = 0
+        self.latency = 0
+        self.success = 0
+
+    def __str__(self):
+        return "Name : {0},Nb rx : {1},Nb tx : {2},Pdr : {3},Average transmission latency : {4},Send success : {5}".format(self.name, len(self.list_rx), len(self.list_tx), self.pdr, self.latency, self.latency, self.success)
+
+    def getIdsRx(self):
+        """Return the id of this node's transmitted messages
+        
+        Return
+        ------
+            list
+                list of transmitted messages' ids
+        """
+        return set([rx.msg_id for rx in self.list_rx])
+
+    def getIdsTx(self):
+        """Return the id of this node's received messages
+        
+        Return
+        ------
+            list
+                list of received messages' ids
+        """
+
+        return set([tx.msg_id for tx in self.list_tx])
+
+class Message:
+    """
+    Represent a message in the log file
+    
+    Attributes
+    ----------
+    timestamp : int
+        the timestamp of the message
+    msg_id : int
+        this message id
+    node_name : string
+        the node's name that have received or transmitted this message
+    """
+
+    def __init__(self, timestamp, msg_id, node_name):
+        self.timestamp = timestamp
+        self.msg_id = msg_id
+        self.node_name = node_name
+
+class MessageRx(Message):
+    """
+    Represent a received message in the log file
+    
+    Attributes
+    ----------
+    timestamp : int
+        the timestamp of the message
+    msg_id : int
+        this message id
+    node_name : string
+        the node's name that have received or transmitted this message
+    latency : int
+        the latency of this received message (the time it has taken to arrive)
+    """
+
+    def __init__(self, timestamp, msg_id, node_name):
+        Message.__init__(self, timestamp, msg_id, node_name)
+        self.latency = 0
+
+    def __str__(self):
+        return "received;{0};{1};{2};{3}".format(self.node_name, self.timestamp, self.msg_id, self.latency)
+
+class MessageTx(Message):
+    """
+    Represent a received message in the log file
+    
+    Attributes
+    ----------
+    timestamp : int
+        the timestamp of the message
+    msg_id : int
+        this message id
+    node_name : string
+        the node's name that have received or transmitted this message
+    success : int
+        number of nodes that have received this message
+    """
+
+    def __init__(self, timestamp, msg_id, node_name):
+        Message.__init__(self, timestamp, msg_id, node_name)
+        self.success = 0
+
+    def __str__(self):
+        return "transmitted;{0};{1};{2};{3}".format(self.node_name, self.timestamp, self.msg_id, self.success)
+
+
+def packetDeliveryRatio(nodes_list):
+    """Add the packet delivery ratio for each nodes in the nodes list
+
+    Parameters
+    ----------
+    nodes_list
+        a list of Node
+
+    Notes
+    -----
+    Packet delivery ratio = ∑ Number of packet received / ∑ Number of packet send
+    
+    """
+    # Total of received nodes (for all nodes)
+    total_tx = sum([len(n.list_tx) for n in nodes_list])
+    for node in nodes_list:
+        # Node pdr (%): Total received msg for this node / Total transmitted msg by other nodes
+        node.pdr = (len(node.list_rx) / ( total_tx - len(node.list_tx)))*100
+
+def averageTransmissionLatency(nodes_list):
+    """Add the average transmission latency for each nodes in the nodes list
+
+    Parameters
+    ----------
+    nodes_list
+        a list of Node
+
+    Notes
+    -----
+    The time is in ms
+    The average time a transmitted msg take to be received by an other node
+    
+    """
+    for n in nodes_list:
+        n.sucess = 0 # Nb of time a transmitted msg has been successfully received by a node
+        latency = 0 # Total latency of the transmitted message by node n
+
+        # For all transmitted msg of node n
+        for tx in n.list_tx:
+            # For all the other node n2 
+            for n2 in nodes_list:
+                # For all the received msg of n2
+                for rx in n2.list_rx:
+                    # If a received msg of n2 has the same id 
+                    # than a transmitted message from n
+                    if tx.msg_id == rx.msg_id:
+                        n.success += 1
+                        tx.success += 1
+                        # We compute the latency between the transmission time of the msg
+                        # and its reception time
+                        rx.latency = rx.timestamp - tx.timestamp
+                        latency += rx.timestamp - tx.timestamp
+        if n.success:
+            # We compute the average transmission latency by nodes
+            n.latency = latency / n.success 
+
+
